fe4e25ee9896fa826799add90b22f897fdc9ceed
Dan Cavallaro <dtc008@bucknell.edu>
2014-11-28 11:32:38 -0800

    Adding logging

--- app/racktest.rb
+++ app/racktest.rb
@@ -1,7 +1,16 @@
+require 'logger'
+
 module App
   class RackTest
 
+    def initialize
+      @logger = Logger.new(STDOUT)
+    end
+
     def call(env)
+      request = Rack::Request.new(env)
+      @logger.info "RackTest serving request on #{request.path}"
+
       [200, {"Content-Type" => "text/html"}, ['A barebones rack app']]
     end
 
